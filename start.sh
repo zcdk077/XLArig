b564712b128ce0d20800160cb1b8d0cf85c1c303
zcdk077 <zackynautika@gmail.com>
2025-01-18 18:31:02 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+echo "Siap memulai mining ??"
+sleep 2
+echo "Mempersiapkan konfigurasi ."
+sleep 2
+echo "Mempersiapkan konfigurasi . ."
+sleep 2
+echo "Mempersiapkan konfigurasi . . ."
+sleep 2
+echo "Memasang konfigurasi"
+sleep 2
+echo "Salam Receh Bosku . . . ."
+sleep 2
+echo "Mining dimulai dalam hitungan ke 5"
+sleep 1
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+clear
+
+~/XLArig/build/xlarig -c ~/XLArig/config.json
